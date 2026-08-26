511b96baada702c5798f05c0581319b7f836deac
arifff6 <arifm@students.amikom.ac.id>
2026-08-26 15:55:18 +0700

    fix complete request lifecycle for vercel

--- api/index.php
+++ api/index.php
@@ -1,8 +1,13 @@
 <?php
 
-$storageDirs = [
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Buat direktori wajib di /tmp
+$dirs = [
     '/tmp/storage/app',
-    '/tmp/storage/framework/cache',
     '/tmp/storage/framework/cache/data',
     '/tmp/storage/framework/sessions',
     '/tmp/storage/framework/views',
@@ -10,16 +15,39 @@ $storageDirs = [
     '/tmp/bootstrap/cache',
 ];
 
-foreach ($storageDirs as $dir) {
+foreach ($dirs as $dir) {
     if (!is_dir($dir)) {
         mkdir($dir, 0777, true);
     }
 }
 
+// Redirect path cache ke /tmp
+putenv('APP_STORAGE=/tmp/storage');
 putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
 putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
-putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+putenv('APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php');
 putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
 putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes-v7.php');
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
 
-require __DIR__ . '/../public/index.php';
+// Autoload composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Atur storage path ke /tmp
+$app->useStoragePath('/tmp/storage');
+
+// Jalankan Request
+if (method_exists($app, 'handleRequest')) {
+    // Laravel 11
+    $app->handleRequest(Request::capture());
+} else {
+    // Laravel versi sebelumnya
+    $kernel = $app->make(Kernel::class);
+    $response = $kernel->handle(
+        $request = Request::capture()
+    )->send();
+    $kernel->terminate($request, $response);
+}
