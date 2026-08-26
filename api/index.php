4dd349930528edc02287b773f32b26d3d5a4e759
arifff6 <arifm@students.amikom.ac.id>
2026-08-26 15:32:39 +0700

    auto create tmp storage folders

--- api/index.php
+++ api/index.php
@@ -1,3 +1,21 @@
 <?php
 
+// Buat direktori sementara yang dibutuhkan Laravel di Vercel
+$storageDirs = [
+    '/tmp/storage',
+    '/tmp/storage/app',
+    '/tmp/storage/framework',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+];
+
+foreach ($storageDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
